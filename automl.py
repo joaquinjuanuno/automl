8ab4b0c979a83c10cd43aa7270a90388fe26f499
AngelV1107 <angelleandrov@gmail.com>
2020-04-30 16:28:08 -0300

    Agregada variable spaces para automl.py

--- automl.py
+++ automl.py
@@ -5,5 +5,6 @@ import sys
 for position in range(50):
     with open(f'drawings/{sys.argv[1]}.txt') as f:
         object_ = f.read().strip()
-    print(f'{" " * position}{object_}', end='\r')
+    spaces = " " * position
+    print(f'{spaces}{object_}', end='\r')
     time.sleep(.1)
