72ee8ee320a0c34f696aec0b868f3e646d3e4f7e
Joaquin Juan Uno <joaquinjuanuno@gmail.com>
2020-04-30 19:49:51 +0000

    Great style improvements

--- automl.py
+++ automl.py
@@ -1,10 +1,9 @@
-import time
 import sys
 
 
 for position in range(50):
-    spaces = " " * position
+    Spaces=" " * position
     with open(f'drawings/{sys.argv[1]}.txt') as f:
         object_ = f.read().strip()
-    print(f'/\{spaces}{object_}', end='\r')
+    print(f'/\{Spaces}{object_}', end='\r')
     time.sleep(.1)
