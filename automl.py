4cd1baa7d134ab723789a67c9efd17ba97167ce0
Joaquin Juan Uno <joaquinjuanuno@gmail.com>
2020-04-30 19:54:41 +0000

    Fixing CI

--- automl.py
+++ automl.py
@@ -1,9 +1,10 @@
+import time
 import sys
 
 
 for position in range(50):
-    Spaces=" " * position
+    spaces = " " * position
     with open(f'drawings/{sys.argv[1]}.txt') as f:
         object_ = f.read().strip()
-    print(f'/\{Spaces}{object_}', end='\r')
+    print(f'/\\{spaces}{object_}', end='\r')
     time.sleep(.1)
