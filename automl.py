bf070ac78561db51d3ff3adef783fd1edb1f5989
Christian Prada <cprada87@gmail.com>
2020-04-30 21:29:43 +0200

    change spaces to variable

--- automl.py
+++ automl.py
@@ -1,9 +1,9 @@
 import time
 import sys
 
-
+spaces = " " * position
 for position in range(50):
     with open(f'drawings/{sys.argv[1]}.txt') as f:
         object_ = f.read().strip()
-    print(f'{" " * position}{object_}', end='\r')
+    print(f'{spaces}{object_}', end='\r')
     time.sleep(.1)
