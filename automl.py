1d3bf8549776a13a676a561d3ae6918cb59f15b0
Joaquin Juan Uno <joaquinjuanuno@gmail.com>
2020-04-30 19:29:37 +0000

    Add departure

--- automl.py
+++ automl.py
@@ -5,5 +5,5 @@ import sys
 for position in range(50):
     with open(f'drawings/{sys.argv[1]}.txt') as f:
         object_ = f.read().strip()
-    print(f'{" " * position}{object_}', end='\r')
+    print(f'/\{" " * position}{object_}', end='\r')
     time.sleep(.1)
