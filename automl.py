9762095ba7c58c30a6c14103a92d192592e8110f
joaquinjuanuno <64608564+joaquinjuanuno@users.noreply.github.com>
2020-04-30 20:41:24 +0100

    Add departure (#20)

--- automl.py
+++ automl.py
@@ -6,5 +6,5 @@ for position in range(50):
     spaces = " " * position
     with open(f'drawings/{sys.argv[1]}.txt') as f:
         object_ = f.read().strip()
-    print(f'{spaces}{object_}', end='\r')
+    print(f'/\{spaces}{object_}', end='\r')
     time.sleep(.1)
