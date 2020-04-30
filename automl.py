19e27331384229f82a2609db3b1fe65bf8f2df5e
Christian Prada <cprada87@gmail.com>
2020-04-30 21:33:42 +0200

    reorder code (#21)

--- automl.py
+++ automl.py
@@ -3,7 +3,8 @@ import sys
 
 
 for position in range(50):
+    spaces = " " * position
     with open(f'drawings/{sys.argv[1]}.txt') as f:
         object_ = f.read().strip()
-    print(f'{" " * position}{object_}', end='\r')
+    print(f'{spaces}{object_}', end='\r')
     time.sleep(.1)
