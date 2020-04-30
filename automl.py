fadbbb9a666356c37048cd5c084ef4e3a1eba514
Joaquin Juan Uno <joaquinjuanuno@gmail.com>
2020-04-30 18:30:32 +0000

    Receiving drawing as parameter

--- automl.py
+++ automl.py
@@ -1,7 +1,9 @@
 import time
+import sys
+
 
 for position in range(50):
-    with open('drawings/marc.txt') as f:
+    with open(f'drawings/{sys.argv[1]}.txt') as f:
         object_ = f.read().strip()
     print(f'{" " * position}{object_}', end='\r')
     time.sleep(.1)
